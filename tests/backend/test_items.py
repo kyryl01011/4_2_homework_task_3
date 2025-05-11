36b184720f971ef54b9d90b7f188d419e4a5314f
Apacha1 <mrpepperjam@gmail.com>
2025-05-11 12:13:52 +0200

    little fixes

--- tests/backend/test_items.py
+++ tests/backend/test_items.py
@@ -1,5 +1,3 @@
-
-
 class TestItems:
 
     def test_delete_all_items(self, auth_session):
@@ -7,8 +5,7 @@ class TestItems:
         for item in items_list:
             auth_session.send_request('DELETE', f'/api/v1/items/{item['id']}')
         new_list = auth_session.send_request('GET', '/api/v1/items/').json().get('data', [])
-        assert new_list == []
-
+        assert new_list == [], f'List is not empty: {new_list}'
 
     def test_successful_item_create(self, auth_session, item_data):
         generated_item_data = item_data()
@@ -21,14 +18,23 @@ class TestItems:
     def test_negative_item_create(self, auth_session, item_data):
         generated_item_data = item_data()
         create_item_response = auth_session.send_request('POST', '/api/v1/items/', {'title': ''}, expected_status_code=422)
-        response_dict = create_item_response.json()
-        assert type(response_dict['detail'][0]['loc'][0]) is str
+        err_message_response = create_item_response.json()['detail'][0]
+        print(err_message_response)
+        assert err_message_response['type'] == 'string_too_short', f'Unexpected error type: {err_message_response['type']}'
+        assert err_message_response['msg'] == 'String should have at least 1 character', f'Unexpected error message: {err_message_response['msg']}'
+        assert err_message_response['input'] == '', f'Unexpected input: {err_message_response['input']}'
+        assert err_message_response['loc'] == ['body', 'title'], f'Unexpected error location: {err_message_response['loc']}'
 
     def test_pagination_with_twenty_items_creation(self, auth_session, item_data):
         created_items_ids = []
         for item in range(20):
             created_item_id = self.test_successful_item_create(auth_session, item_data)['id']
             created_items_ids.append(created_item_id)
+        all_items_response = auth_session.send_request('GET', '/api/v1/items/')
+        all_items_response_data = all_items_response.json().get('data', [])
+        all_items_response_ids = [item['id'] for item in all_items_response_data]
+        for item_id in created_items_ids:
+            assert item_id in all_items_response_ids, f'No ID of created item in existing items data: {item_id}'
         return created_items_ids
 
     def test_get_all_items_list(self, auth_session, item_data):
@@ -42,7 +48,7 @@ class TestItems:
             assert 'description' in item.keys(), f'No key with name "description" in response data dict {item}'
             assert 'id' in item.keys(), f'No key with name "id" in response data dict {item}'
             assert 'owner_id' in item.keys(), f'No key with name "owner_id" in response data dict {item}'
-        assert type(all_items_count) is int, ''
+        assert type(all_items_count) is int, f'Unexpected count response type: {type(all_items_count)}'
 
     def test_full_update_item_by_id(self, auth_session, item_data):
         initial_item = self.test_successful_item_create(auth_session, item_data)
