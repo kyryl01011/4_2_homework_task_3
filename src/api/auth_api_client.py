94e47a7910e1a01c6891d48ba22b83118ad5be38
Apacha1 <mrpepperjam@gmail.com>
2025-06-12 15:35:30 +0200

    refactor

--- src/api/auth_api_client.py
+++ src/api/auth_api_client.py
@@ -1,11 +1,11 @@
 from src.custom_requester.custom_requester import CustomRequester
-from src.enums.url_components import AUTH_DATA, API_HEADERS
+from src.enums.url_components import URLComponents
 
 
 class AuthApi(CustomRequester):
 
     def auth_current_session(self):
-        response = self.send_request('POST', '/api/v1/login/access-token', data=AUTH_DATA)
+        response = self.send_request('POST', '/api/v1/login/access-token', data=URLComponents.AUTH_DATA.value)
         token = response.json()['access_token']
-        self.session.headers.update(API_HEADERS)
+        self.session.headers.update(URLComponents.API_HEADERS.value)
         self.session.headers.update({'Authorization': 'Bearer ' + token})
